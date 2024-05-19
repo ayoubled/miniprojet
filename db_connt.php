d97c2800ad6291711a57effa69e891b94031b244
ayoubled <ayoubled43@gmail.com>
2024-05-19 18:52:35 +0100

    Initial commit

---
+++ db_connt.php
@@ -0,0 +1,11 @@
+<?php
+
+$dashname = "localhost";
+$username="root";
+$password= "";
+$db_name="login";
+$conn= new mysqli($dashname,$username,$password,$db_name);
+if($conn->connect_error)
+{
+    echo" Connection Failed".$conn->connect_error;
+}
